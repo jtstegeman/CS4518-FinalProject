379f37ffeb1332c80c446f16e0424950e6737ca5
msgonsalves <msgonsalves@wpi.edu>
2018-02-24 19:09:03 -0500

    stuff happens on the main screen now

--- app/src/main/java/com/jtstegeman/cs4518_finalproject/CurrentAlarms.java
+++ app/src/main/java/com/jtstegeman/cs4518_finalproject/CurrentAlarms.java
@@ -11,18 +11,34 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.jtstegeman.cs4518_finalproject.database.AlarmHelper;
 import com.jtstegeman.cs4518_finalproject.database.AlarmObject;
 
 import java.util.Date;
+import java.util.List;
 
 public class CurrentAlarms extends AppCompatActivity {
-    @Override
+
+
+    private static final String SAVED_SUBTITLE_VISIBLE = "subtitle";
+
+    private AlarmAdapter mAdapter;
+    private RecyclerView mAlarmRecyclerView;
+    private boolean mSubtitleVisible;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         if(Settings.isFirst(this)){
             Intent i = new Intent(this, TutorialActivity.class);
@@ -46,9 +62,21 @@ public class CurrentAlarms extends AppCompatActivity {
                 a.setLocation("somewhere over the rainbow");
                 AlarmScheduler scheduler = new AlarmScheduler(a);
                 scheduler.schedule(ctx);
+
+                updateUI();
             }
         });
         requestSMS();
+
+
+
+
+        mAlarmRecyclerView = (RecyclerView) this
+                .findViewById(R.id.alarm_recycler_view);
+        mAlarmRecyclerView.setLayoutManager(new LinearLayoutManager(getParent()));
+
+
+        updateUI();
     }
 
     @Override
@@ -58,6 +86,12 @@ public class CurrentAlarms extends AppCompatActivity {
         return true;
     }
 
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -81,6 +115,85 @@ public class CurrentAlarms extends AppCompatActivity {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.SEND_SMS},
                     100);
+        }
+    }
+
+    private void updateUI() {
+        AlarmHelper alarmHelper = AlarmHelper.getInstance(this);
+        List<AlarmObject> alarms = alarmHelper.getAlarms();
+
+
+        if (mAdapter == null) {
+            mAdapter = new AlarmAdapter(alarms);
+            mAlarmRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.setAlarms(alarms);
+            mAdapter.notifyDataSetChanged();
+        }
+
+
+    }
+
+    private class AlarmHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+
+        private AlarmObject mAlarm;
+
+        private TextView mTime;
+        private TextView mLocation;
+
+        public AlarmHolder(View itemView){
+
+            super(itemView);
+            itemView.setOnClickListener(this);
+
+            mTime = (TextView) itemView.findViewById(R.id.dataTime);
+            mLocation = (TextView) itemView.findViewById(R.id.dataLocation);
+        }
+
+        public void bindAlarm(AlarmObject alarm){
+            mAlarm = alarm;
+
+            mTime.setText(mAlarm.getTime().toString());
+            mLocation.setText(mAlarm.getLocation());
+        }
+
+        @Override
+        public void onClick(View v) {
+           // Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
+           // startActivity(intent);
+        }
+    }
+
+    private class AlarmAdapter extends RecyclerView.Adapter<AlarmHolder> {
+
+        private List<AlarmObject> mAlarms;
+
+        public AlarmAdapter(List<AlarmObject> alarms) {
+            mAlarms = alarms;
+        }
+
+        @Override
+        public AlarmHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+
+            View view = getLayoutInflater().inflate(R.layout.list_item_alarm, parent, false);
+            return new AlarmHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(AlarmHolder holder, int position) {
+            AlarmObject alarm = mAlarms.get(position);
+            holder.bindAlarm(alarm);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mAlarms.size();
+        }
+
+        public void setAlarms(List<AlarmObject> alarms) {
+            mAlarms = alarms;
         }
     }
 }
